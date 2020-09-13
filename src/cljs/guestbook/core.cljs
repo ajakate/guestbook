cd785254330d5e693dc934d89723d776600acf8e
Ajay Jakate <ajay.jakate@gmail.com>
2020-09-13 18:31:36 -0500

    error handling via POST

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,31 +1,43 @@
 (ns guestbook.core
   (:require [reagent.core :as r]
             [reagent.dom  :as dom]
-            [ajax.core :refer [GET POST]]))
+            [ajax.core :refer [GET POST]]
+            [clojure.string :as string]))
 
-(defn send-message! [fields]
+(defn send-message! [fields errors]
   (POST "/message"
     {:params @fields
      :headers
      {"Accept" "application/transit+json"
-    ;   "x-csrf-token" (.-value (.getElementById js/document "token"))}
       "x-csrf-token" (-> js/document (.getElementById "token") .-value)}
-     :handler #(.log js/console (str "response:" %))
-     :error-handler #(.error js/console (str "error:" %))}))
+     :handler #(do
+                 (.log js/console (str "response: " %))
+                 (reset! errors nil))
+     :error-handler #(do
+                       (.error js/console (str "error:" %))
+                       (reset! errors (get-in % [:response :errors])))}))
+
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+    [:div.notification.is-danger (string/join error)]))
 
 (defn message-form []
-  (let [fields (r/atom {})]
+  (let [fields (r/atom {})
+        errors (r/atom nil)]
     (fn [] [:div
             [:p "Name: " (:name @fields)]
             [:p "Message: " (:message @fields)]
+            [errors-component errors :server-error]
             [:div.field
              [:label.label {:for :name} "Name"]
+             [errors-component errors :name]
              [:input.input
               {:type :text
                :name :name
                :on-change #(swap! fields assoc :name (-> % .-target .-value)) :value (:name @fields)}]]
             [:div.field
              [:label.label {:for :message} "Message"]
+             [errors-component errors :message]
              [:textarea.textarea
               {:name :message
                :value (:message @fields)
@@ -33,7 +45,7 @@
             [:input.button.is-primary
              {:type :submit
               :value "comment"
-              :on-click #(send-message! fields)}]])))
+              :on-click #(send-message! fields errors)}]])))
 
 (defn home []
   [:div.content>div.columns.is-centered>div.column.is-two-thirds
